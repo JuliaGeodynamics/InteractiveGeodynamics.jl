22710820c2506e3a1391a544a33d9364013548e8
Boris Kaus <kaus@uni-mainz.de>
2023-05-15 21:47:55 +0200

    min or adjustments

--- src/Basic_LaMEM_GUI.jl
+++ src/Basic_LaMEM_GUI.jl
@@ -248,12 +248,15 @@ function Create_Basic_LaMEM_GUI(OutFile, ParamFile; resolution = nothing, fontsi
             if (it==0) && (length(gui.menu.options[])==2)
                 update_fields_menu(OutFile, gui.menu)   
             end
-            it += 1
+         
             
             # Update the plots etc.
             sleep(0.5)
-            t_step, gui = update_plot_info(OutFile, gui, t_step, last=true)
+            if (it>0)
+                t_step, gui = update_plot_info(OutFile, gui, t_step, last=true)
+            end
 
+            it += 1
             println("Timestep $t_step")
         end
         buttonplay.label="Play"
