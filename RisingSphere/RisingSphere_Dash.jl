4f9611eb825218822828f34b5aac94a6566e2adf
Boris Kaus <kaus@uni-mainz.de>
2023-09-12 10:28:56 +0200

    add initial commit

---
+++ RisingSphere/RisingSphere_Dash.jl
@@ -0,0 +1,42 @@
+using Dash, DashBootstrapComponents
+using PlotlyJS
+
+
+
+
+#app = dash(external_stylesheets=[dbc_themes.CYBORG])
+app = dash(external_stylesheets = [dbc_themes.BOOTSTRAP], prevent_initial_callbacks=false)
+
+app.layout = html_div() do
+    dbc_container(className = "mxy-auto", fluid=true, [
+        dbc_col(html_h1("Rising Sphere example"), style = Dict("margin-top" => 0, "textAlign" => "center")),
+        
+        
+        dbc_row([dbc_col(
+            dcc_graph(
+                id = "example-graph-1",
+                figure = (
+                    data = [
+                        (x = ["giraffes", "orangutans", "monkeys"], y = [20, 14, 23], type = "bar", name = "SF"),
+                        (x = ["giraffes", "orangutans", "monkeys"], y = [12, 18, 29], type = "bar", name = "Montreal"),
+                    ],
+                    layout = (title = "Dash Data Visualization", barmode="group")
+                ),
+                #animate   = false,
+                #responsive=false,
+                #clickData = true,
+                #config = PlotConfig(displayModeBar=false, scrollZoom = false),
+                style = attr(width="80vw", height="80vh",padding_left="0vw",)
+
+                )),
+            dbc_col(dbc_button("RUN", id="button-run"),width=2)
+        
+        ])
+
+    ])
+end
+
+
+#app.layout = main_layout()
+    
+run_server(app, debug=false)
