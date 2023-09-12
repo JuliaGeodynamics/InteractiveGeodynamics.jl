2f0111bdfb70d6f449a999b0926c99dd72092ccf
Jamison Assunção <jamison.assuncao@gmail.com>
2023-09-12 15:17:04 +0200

    added more parameters

--- RisingSphere/RisingSphere_Dash.jl
+++ RisingSphere/RisingSphere_Dash.jl
@@ -36,52 +36,55 @@ app.title = title_app
 
 # Main code layout
 app.layout = html_div() do
-    dbc_container(className = "mxy-auto", fluid=true, [
-        dbc_col(html_h1(title_app), style = Dict("margin-top" => 0, "textAlign" => "center")),
-        
-        dbc_row([
-                dbc_col([dbc_col(create_main_figure()),
-                        dbc_col(dbc_label("", id="label-id"))]),      # main figure window
+    dbc_container(className="mxy-auto", fluid=true, [
+        dbc_col(html_h1(title_app), style=Dict("margin-top" => 0, "textAlign" => "center")), dbc_row([
+            dbc_col([dbc_col(create_main_figure()),
+                dbc_col(dbc_label("", id="label-id"))]),      # main figure window
 
-                # right side menu
-                dbc_col([   dbc_card([dbc_col(dbc_label("Time: 0 Myrs", id="label-time")),
-                                      dbc_col(dbc_label("Timestep: 0", id="label-timestep"))]),
-
-                            dbc_card(dbc_label("Parameters", id="Parameters", size="lg")),
-                            
-                            dbc_card([
-                                    dbc_col(dbc_label("Density of Sphere", id="density_sphere_label")),
-                                    dbc_col(dbc_input(id="sphere_density", placeholder="Insert the sphere density", type="number"))]),
-                            dbc_card([
-                                    dbc_col(dbc_label("Density of Matrix", id="density_matrix_label")),
-                                    dbc_col(dbc_input(id="matrix_density", placeholder="Insert the matrix density", type="number"))]),
-                            dbc_card([
-                                    dbc_col(dbc_label("Radius of Sphere", id="sphere_radius_label")),
-                                    dbc_col(dbc_input(id="sphere_radius", placeholder="Insert the radius of the sphere", type="number"))]),
-                            dbc_card([
-                                    dbc_col(dbc_label("Width of Domain", id="domain_width_label")),
-                                    dbc_col(dbc_input(id="domain_width", placeholder="Insert the width of the domain", type="number"))]),
-                            dbc_col(dbc_button("RUN", id="button-run", size="lg", class_name="d-grid gap-2 col-12 mx-auto")),
-                                    ],
-
-                            
-                            width=2)
-                        
-
-        
-        ]),
+            # right side menu
+            dbc_col([dbc_card([dbc_label(" Time: 0 Myrs", id="label-time"),
+                            dbc_label(" Timestep: 0", id="label-timestep")], color="secondary", outline=true),
+                    dbc_card(dbc_label("Parameters", id="Parameters", size="lg"), color="white", outline=true),
+                    dbc_card([
+                            dbc_label("# of elements in the x-direction: ", id="nel_x_label"),
+                            dbc_input(id="nel_x", placeholder="Insert the # of elements in the x-direction", type="number", min=2),
+                        ], color="white", outline=true),
+                    dbc_card([
+                            dbc_label("# of elements in the z-direction: ", id="nel_z_label"),
+                            dbc_input(id="nel_z", placeholder="Insert the # of elements in the z-direction", type="number", min=2),
+                        ], color="white", outline=true),
+                    dbc_card([
+                            dbc_label("# of timesteps: ", id="n_timesteps_label"),
+                            dbc_input(id="n_timesteps", placeholder="Insert the # of timesteps", type="number", min=1),
+                        ], color="white", outline=true),
+                    dbc_card([
+                            dbc_label("Density of Sphere: ", id="density_sphere_label"),
+                            dbc_input(id="sphere_density", placeholder="Insert the sphere density", type="number", min=1
+                            )], color="white", outline=true),
+                    dbc_card([
+                            dbc_label("Density of Matrix: ", id="density_matrix_label"),
+                            dbc_input(id="matrix_density", placeholder="Insert the matrix density", type="number", min=1)
+                        ], color="white", outline=true),
+                    dbc_card([
+                            dbc_label("Radius of Sphere: ", id="sphere_radius_label"),
+                            dbc_input(id="sphere_radius", placeholder="Insert the radius of the sphere", type="number")
+                        ], color="white", outline=true),
+                    dbc_card([
+                            dbc_label("Width of Domain: ", id="domain_width_label"),
+                            dbc_input(id="domain_width", placeholder="Insert the width of the domain", type="number")
+                        ], color="white", outline=true),
+                    dbc_col(dbc_button("RUN", id="button-run", size="lg", class_name="d-grid gap-2 col-12 mx-auto"))
+                ],
+                width=2)]),
 
         # Store a unique number of our session in the webpage
-        dcc_store(id="session-id", data =  ""),     
-        dcc_store(id="current_timestep", data =  "0"),     
+        dcc_store(id="session-id", data=""),
+        dcc_store(id="current_timestep", data="0"),
 
         # Start an interval that updates the number every second
-        dcc_interval(id="session-interval", interval=1000, n_intervals=0, disabled=true)     
-
-    ])
+        dcc_interval(id="session-interval", interval=1000, n_intervals=0, disabled=true)])
 
 end
-
 
 # This creates an initial session id that is unique for this session
 # it will run on first start 
@@ -101,13 +104,16 @@ callback!(app,
     Output("label-timestep", "children"),
     Output("session-interval","disabled"),
     Input("button-run", "n_clicks"),
+    State("nel_x", "value"),
+    State("nel_z", "value"),
+    State("n_timesteps", "value"),
     State("sphere_density", "value"),
     State("matrix_density", "value"),
     State("sphere_radius", "value"),
     State("domain_width", "value"),
     prevent_initial_call=true
-) do n_run, input_density, input_matrix, input_radius, input_width
-    @show n_run, input_density, input_matrix, input_radius, input_width
+) do n_run, nel_x, nel_z, n_timesteps, input_density, input_matrix, input_radius, input_width
+    @show n_run, nel_x, nel_z, n_timesteps, input_density, input_matrix, input_radius, input_width
     str = "Timestep: $n_run"
 
 
