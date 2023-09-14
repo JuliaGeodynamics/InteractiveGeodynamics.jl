5fa97075afdd03b6ba8820a98165db66d639d91f
Jamison Assunção <jamison.assuncao@gmail.com>
2023-09-14 11:31:50 +0200

    clean up 2

--- RisingSphere/RisingSphere_Dash.jl
+++ RisingSphere/RisingSphere_Dash.jl
@@ -80,7 +80,6 @@ callback!(app,
     return String("$(session_id)"), str
 end
 
-
 # Call run button
 callback!(app,
     Output("session-interval", "disabled"),
@@ -127,21 +126,20 @@ callback!(app,
     return disable_interval
 end
 
-
 # deactivate the button 
 callback!(app,
-    Output("button-run","disabled"),
-    Output("button-run","color"),
+    Output("button-run", "disabled"),
+    Output("button-run", "color"),
     Input("button-run", "n_clicks"),
     Input("session-interval", "n_intervals"),
-    State("last_timestep","data"),
-    State("current_timestep","data"),
+    State("last_timestep", "data"),
+    State("current_timestep", "data"),
     prevent_initial_call=true
 ) do n_run, n_inter, last_timestep, current_timestep
 
     cur_t = parse(Int, current_timestep)    # current timestep
     last_t = parse(Int, last_timestep)      # last timestep available on disk
-    if cur_t<last_t 
+    if cur_t < last_t
         button_run_disable = true
         button_color = "danger"
     else
@@ -152,27 +150,26 @@ callback!(app,
     return button_run_disable, button_color
 end
 
-
 # Check if *.pvd file on disk changed and a new timestep is available
 callback!(app,
     Output("last_timestep", "data"),
-    Output("update_fig","data"),
+    Output("update_fig", "data"),
     Input("session-interval", "n_intervals"),
     Input("button-run", "n_clicks"),
-    State("current_timestep","data"),    
-    State("update_fig","data"),
+    State("current_timestep", "data"),
+    State("update_fig", "data"),
     State("session-id", "data"),
     prevent_initial_call=true
 ) do n_inter, n_run, current_timestep, update_fig, session_id
     trigger = get_trigger()
     if trigger == "session-interval.n_intervals"
-        if isfile(OutFile*".pvd")
+        if isfile(OutFile * ".pvd")
             # Read LaMEM *.pvd file
             Timestep, _, Time = Read_LaMEM_simulation(OutFile)
 
             # Update the labels and data stored in webpage about the last timestep
-            last_time  = "$(Timestep[end])"
-            
+            last_time = "$(Timestep[end])"
+
             update_fig = "$(parse(Int,update_fig)+1)"
         else
             last_time = "0"
@@ -187,33 +184,32 @@ callback!(app,
     return last_time, update_fig
 end
 
-
 # Update the figure if the signal is given to do so
 callback!(app,
     Output("label-timestep", "children"),
     Output("label-time", "children"),
-    Output("current_timestep","data"),
+    Output("current_timestep", "data"),
     Output("figure_main", "figure"),
-    Output("plot_field","options"),
+    Output("plot_field", "options"),
     Output("contour_option", "options"),
-    Input("update_fig","data"),
-    Input("current_timestep","data"),
+    Input("update_fig", "data"),
+    Input("current_timestep", "data"),
     Input("button-run", "n_clicks"),
     Input("button-start", "n_clicks"),
     Input("button-last", "n_clicks"),
     Input("button-forward", "n_clicks"),
     Input("button-back", "n_clicks"),
     Input("button-play", "n_clicks"),
-    State("last_timestep","data"),
+    State("last_timestep", "data"),
     State("session-id", "data"),
-    State("plot_field","value"),
-    State("switch-contour","value"),
-    State("contour_option","value"),
-    State("switch-velocity","value"),
+    State("plot_field", "value"),
+    State("switch-contour", "value"),
+    State("contour_option", "value"),
+    State("switch-velocity", "value"),
     State("color_map_option", "value"),
     prevent_initial_call=true
-) do update_fig, current_timestep,  n_run, n_start, n_last, n_back, n_forward, n_play, last_timestep, session_id, 
-    plot_field, switch_contour, contour_field, switch_velocity, color_map_option
+) do update_fig, current_timestep, n_run, n_start, n_last, n_back, n_forward, n_play, last_timestep, session_id,
+plot_field, switch_contour, contour_field, switch_velocity, color_map_option
 
     trigger = get_trigger()
 
@@ -222,15 +218,15 @@ callback!(app,
     last_t = parse(Int, last_timestep)                      # last timestep available on disk
     fig_cross = []
     fields_available = ["phase"]
-    if trigger == "current_timestep.data" || 
-        trigger == "update_fig.data" ||
-        trigger == "button-start.n_clicks" ||
-        trigger == "button-last.n_clicks" ||
-        trigger == "button-back.n_clicks" ||
-        trigger == "button-forward.n_clicks" ||
-        trigger == "button-play.n_clicks"
+    if trigger == "current_timestep.data" ||
+       trigger == "update_fig.data" ||
+       trigger == "button-start.n_clicks" ||
+       trigger == "button-last.n_clicks" ||
+       trigger == "button-back.n_clicks" ||
+       trigger == "button-forward.n_clicks" ||
+       trigger == "button-play.n_clicks"
 
-        if isfile(OutFile*".pvd")
+        if isfile(OutFile * ".pvd")
             Timestep, _, Time = Read_LaMEM_simulation(OutFile)      # all timesteps
             id = findall(Timestep .== cur_t)[1]
             if trigger == "button-start.n_clicks" || trigger == "button-play.n_clicks"
@@ -239,25 +235,25 @@ callback!(app,
             elseif trigger == "button-last.n_clicks"
                 cur_t = Timestep[end]
                 id = length(Timestep)
-            elseif (trigger == "button-forward.n_clicks") && (id<length(Timestep))
+            elseif (trigger == "button-forward.n_clicks") && (id < length(Timestep))
                 cur_t = Timestep[id+1]
-                id = id+1
-            elseif (trigger == "button-back.n_clicks") && (id>1)
+                id = id + 1
+            elseif (trigger == "button-back.n_clicks") && (id > 1)
                 cur_t = Timestep[id-1]
-                id = id-1
+                id = id - 1
             end
 
             # Load data 
-            x,y,data, time, fields_available = get_data(OutFile, cur_t, plot_field)
+            x, y, data, time, fields_available = get_data(OutFile, cur_t, plot_field)
             if !isnothing(switch_contour)
                 add_contours = true
-                x_con,y_con,data_con, _, _ = get_data(OutFile, cur_t, contour_field)
+                x_con, y_con, data_con, _, _ = get_data(OutFile, cur_t, contour_field)
             else
-                x_con, y_con, data_con = x,y,data
+                x_con, y_con, data_con = x, y, data
                 add_contours = false
-            end    
+            end
             # update the plot
-            
+
             if isnothing(switch_velocity)
                 add_velocity = false
             else
@@ -265,13 +261,13 @@ callback!(app,
             end
 
 
-            fig_cross = create_main_figure(OutFile, cur_t,x,y,data, x_con, y_con, data_con, field=plot_field, cmaps; 
-                            add_contours = add_contours, contour_field = contour_field,
-                            add_velocity = add_velocity,
-                            colorscale   = color_map_option)
+            fig_cross = create_main_figure(OutFile, cur_t, x, y, data, x_con, y_con, data_con, field=plot_field, cmaps;
+                add_contours=add_contours, contour_field=contour_field,
+                add_velocity=add_velocity,
+                colorscale=color_map_option)
 
-            if trigger == "current_timestep.data" ||  trigger == "update_fig.data" ||  trigger == "button-play.n_clicks"
-                if cur_t < last_t 
+            if trigger == "current_timestep.data" || trigger == "update_fig.data" || trigger == "button-play.n_clicks"
+                if cur_t < last_t
                     cur_t = Timestep[id+1]      # update current timestep
                 end
             end
@@ -287,9 +283,9 @@ callback!(app,
 
     # update the labels
     label_timestep = "Timestep: $cur_t"
-    label_time="Time: $time Myrs"
+    label_time = "Time: $time Myrs"
     current_timestep = "$cur_t"
-    
+
     @show current_timestep
     return label_timestep, label_time, current_timestep, fig_cross, fields_available, fields_available
 end
@@ -297,19 +293,17 @@ end
 # 
 callback!(app,
     Output("contour_option", "disabled"),
-    Input("switch-contour", "value")) do  switch_contour
+    Input("switch-contour", "value")) do switch_contour
     if !isnothing(switch_contour)
         if isempty(switch_contour)
-            disable_contours = true    
+            disable_contours = true
         else
-            disable_contours = false    
+            disable_contours = false
         end
     else
-        disable_contours = true    
+        disable_contours = true
     end
-    return disable_contours    
+    return disable_contours
 end
-
-
 
 run_server(app, debug=false)
